722105c9d705cff78c7695d1edee14b6c6b6f540
wawanneutron <wawanneutron1331@gmail.com>
2023-10-23 16:38:12 +0700

    we stand with Palestine

--- script/script.js
+++ script/script.js
@@ -1,30 +1,33 @@
 /* native js navbar slider togler */
-const body = document.querySelector("body");
-const menu = document.querySelector(".navbar-nav");
-const icon = document.querySelector(".icon-social");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
+const body = document.querySelector('body')
+const menu = document.querySelector('.navbar-nav')
+const icon = document.querySelector('.icon-social')
+const menuBtn = document.querySelector('.menu-btn')
+const cancelBtn = document.querySelector('.cancel-btn')
 
 menuBtn.onclick = () => {
-  menu.classList.add("active");
-  icon.classList.add("hide");
-  menuBtn.classList.add("hide");
-  body.classList.add("disabledScroll");
-};
+  menu.classList.add('active')
+  icon.classList.add('hide')
+  menuBtn.classList.add('hide')
+  body.classList.add('disabledScroll')
+}
 cancelBtn.onclick = () => {
-  menu.classList.remove("active");
-  icon.classList.remove("active");
-  menuBtn.classList.remove("hide");
-  body.classList.remove("disabledScroll");
-};
+  menu.classList.remove('active')
+  icon.classList.remove('active')
+  menuBtn.classList.remove('hide')
+  body.classList.remove('disabledScroll')
+}
 
 // navbar scrol sticky
 $(window).scroll(function () {
   if (this.scrollY > 100) {
-    $(".navbar").addClass("sticky");
-    $(".navbar-brand img").attr("src", "/images/logo_neutron_sticky.svg");
+    $('.navbar').addClass('sticky')
+    $('.navbar').removeClass('margin')
+
+    $('.navbar-brand img').attr('src', '/images/logo_neutron_sticky.svg')
   } else {
-    $(".navbar").removeClass("sticky");
-    $(".navbar-brand img").attr("src", "/images/logo_neutron.svg");
+    $('.navbar').removeClass('sticky')
+    $('.navbar').addClass('margin')
+    $('.navbar-brand img').attr('src', '/images/logo_neutron.svg')
   }
-});
+})
